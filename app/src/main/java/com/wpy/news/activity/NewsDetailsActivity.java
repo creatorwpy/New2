97c8fefa4b2667cfeec823538152603fcc82e15f
wpy <88ddqq@163.com>
2017-04-27 19:52:33 +0800

    a

--- app/src/main/java/com/wpy/news/activity/NewsDetailsActivity.java
+++ app/src/main/java/com/wpy/news/activity/NewsDetailsActivity.java
@@ -2,6 +2,7 @@ package com.wpy.news.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -13,6 +14,8 @@ import com.wpy.news.base.BaseActivity;
 import com.wpy.news.util.LogDebug;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.ButterKnife;
 
@@ -23,6 +26,7 @@ import butterknife.ButterKnife;
 public class NewsDetailsActivity extends BaseActivity {
     WebView webText;
     LinearLayout loading;
+    private Boolean isOnPause;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -75,6 +79,66 @@ public class NewsDetailsActivity extends BaseActivity {
     /*当窗口关闭的时候也将浏览器关闭，否则视频的声音不停下来*/
     protected void onStop() {
         super.onStop();
-        webText.destroy();
+        LogDebug.e("onStop","onStop");
+//        webText.destroy();
+    }
+
+    /**
+     * 当Activity执行onPause()时让WebView执行pause 
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            if (webText != null) {
+                webText.getClass().getMethod("onPause").invoke(webText, (Object[]) null);
+                isOnPause = true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 当Activity执行onResume()时让WebView执行resume 
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            if (isOnPause) {
+                if (webText != null) {
+                    webText.getClass().getMethod("onResume").invoke(webText, (Object[]) null);
+                }
+                isOnPause = false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 该处的处理尤为重要: 
+     * 应该在内置缩放控件消失以后,再执行webText.destroy() 
+     * 否则报错WindowLeaked 
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (webText != null) {
+            webText.getSettings().setBuiltInZoomControls(true);
+            webText.setVisibility(View.GONE);
+            long delayTime = ViewConfiguration.getZoomControlsTimeout();
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    webText.destroy();
+                    webText = null;
+                }
+            }, delayTime);
+
+        }
+        isOnPause = false;
     }
 }
